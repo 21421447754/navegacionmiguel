8fe4ec7fbb1015d83c1888dd9653cf55c4788a6d
miguel <martinezalfonso583@gmail.com>
2022-05-30 11:45:39 -0500

    Initial commit 1

---
+++ app/src/main/java/martinez/miguel/navegacionmiguel/primerFragment.kt
@@ -0,0 +1,23 @@
+package martinez.miguel.navegacionmiguel
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+
+class primerFragment : Fragment() {
+
+lateinit var boton:Button
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val vista = inflater.inflate(R.layout.fragment_primer, container, false)
+        return vista
+    }
+
+    }
---
+++ app/src/main/java/martinez/miguel/navegacionmiguel/segundoFragment.kt
@@ -0,0 +1,24 @@
+package martinez.miguel.navegacionmiguel
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+
+class segundoFragment : Fragment() {
+
+    lateinit var boton: Button
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val vista = inflater.inflate(R.layout.fragment_segundo, container, false)
+        return vista
+    }
+
+
+            }
